cad2eff143ec253cdd7a909bb7f946448bc550aa
khaalid subaan <48145776+Khaalidsub@users.noreply.github.com>
2021-06-12 17:03:47 +0800

    Add chat event consumer

--- services/apps/chat/src/chat.event.ts
+++ services/apps/chat/src/chat.event.ts
@@ -1,6 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { IBooking } from '@apps/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Ctx, EventPattern, MessagePattern, Payload, RedisContext } from '@nestjs/microservices';
 import { ChatService } from './chat.service';
 @Injectable()
 export class ChatEvent {
+  protected readonly logger = new Logger(ChatEvent.name);
   constructor(private readonly chatService: ChatService) {}
+  @EventPattern('booking_created')
+  onBookingCreate(@Payload() data: IBooking, ) {
+    this.logger.log(`booking of id ${data.id}`)
+    // schedule the chat state an event
+  // console.log(`Channel: ${context.getChannel()}`);
+}
 }
